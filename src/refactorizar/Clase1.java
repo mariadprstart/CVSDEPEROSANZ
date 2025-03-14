865dd02a87fd38250ab0d53d996c6e494217971f
mariadprstart <mariadprstart@hotmail.com>
2025-03-14 12:19:53 +0100

    Primera confirmacion

---
+++ src/refactorizar/Clase1.java
@@ -0,0 +1,15 @@
+
+
+package refactorizar;
+
+
+public class Clase1 {
+
+    
+    public static void main(String[] args) {
+        String nombre="María De Perosanz Ruiz";
+        System.out.println("Este es el proyecto de "+ nombre);
+        
+    }
+    
+}
