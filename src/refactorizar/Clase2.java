10d305282dfdd272480de68bc31675f31b694f39
mariadprstart <mariadprstart@hotmail.com>
2025-03-14 13:44:42 +0100

    Segunda confirmacion

---
+++ src/refactorizar/Clase2.java
@@ -0,0 +1,11 @@
+/* */
+package refactorizar;
+
+public class Clase2 {
+
+    public void visualizarRama() {
+        String rama = "Producción";
+        System.out.println("Estoy en la rama" + rama);
+    }
+
+}
